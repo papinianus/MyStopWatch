919bd27912dd76357ed7ae2008ae7398aeeb777d
papinianus <dglmsksooah@gmail.com>
2024-04-30 20:23:34 +0900

    beep added

--- MyStopWatch/MainForm.cs
+++ MyStopWatch/MainForm.cs
@@ -2,6 +2,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using MyStopWatch.Models;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Media;
+using System.Runtime.InteropServices;
 
 namespace MyStopWatch
 {
@@ -44,6 +47,14 @@ namespace MyStopWatch
             {
                 return;
             }
+
+            var fInfo = new FlashWindowInfo { dwFlags = FlashWindowAll, uCount = 3, dwTimeout = 0 };
+            fInfo.cbSize = Convert.ToUInt32(Marshal.SizeOf(fInfo));
+            fInfo.WindowHandle = GetMainWindowHandle(Program.ProjectName());
+
+
+            _ = FlashWindowEx(ref fInfo);
+            SystemSounds.Beep.Play();
             Model.ToggleStartStop();
             Draw();
         }
@@ -131,5 +142,40 @@ namespace MyStopWatch
             WorksGrid.Refresh();
             WorkList.Refresh();
         }
+
+        [DllImport("user32.dll")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA5392:P/Invoke に対して DefaultDllImportSearchPaths 属性を使用する", Justification = "<保留中>")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "SYSLIB1054:コンパイル時に P/Invoke マーシャリング コードを生成するには、'DllImportAttribute' の代わりに 'LibraryImportAttribute' を使用します", Justification = "<保留中>")]
+        private static extern int FlashWindowEx(ref FlashWindowInfo pwFlashWindowInfo);
+
+
+
+        // 点滅を止める
+        public const uint FlashWindowStop = 0;
+        // タイトルバーを点滅させる
+        public const uint FlashWindowCaption = 1;
+        // タスクバー・ボタンを点滅させる
+        public const uint FlashWindowTray = 2;
+        // タスクバー・ボタンとタイトルバーを点滅させる
+        public const uint FlashWindowAll = 3;
+        // FlashWindowStop が指定されるまでずっと点滅させる
+        public const uint FlashWindowUntilStop = 4;
+        // ウィンドウが最前面に来るまでずっと点滅させる
+        public const uint FlashWindowUntilForeGround = 12;
+
+        public static nint GetMainWindowHandle(string processName) =>
+            Process.GetProcessesByName(processName).FirstOrDefault(x => x.MainWindowHandle != nint.Zero)
+                ?.MainWindowHandle ?? nint.Zero;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1815:equals および operator equals を値型でオーバーライドします", Justification = "<保留中>")]
+    public struct FlashWindowInfo
+    {
+        public uint cbSize;    // FlashWindowInfo 構造体のサイズ
+        public nint WindowHandle;      // 点滅対象のウィンドウ・ハンドル
+        public uint dwFlags;   // FlashWindow 定数のいずれか
+        public uint uCount;    // 点滅する回数
+        public uint dwTimeout; // 点滅する間隔（ミリ秒単位）
     }
 }
